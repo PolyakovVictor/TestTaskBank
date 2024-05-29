472ffdc6cd48cb1bdaac6c80ae7356e211d8671f
Victor Polyakov <polyakov.victor21@gmail.com>
2024-05-29 16:04:09 +0300

    added sort in tables

--- frontend/src/components/BanksTable/index.tsx
+++ frontend/src/components/BanksTable/index.tsx
@@ -1,24 +1,43 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Table, TableBody, TableCell, TableContainer, TableHead, TableRow, IconButton, Paper } from '@mui/material';
 import EditIcon from '@mui/icons-material/Edit';
 import DeleteIcon from '@mui/icons-material/Delete';
 import { BanksTableProps, IBank } from '../../models/interfaces';
 
 const BanksTable: React.FC<BanksTableProps> = ({ banks, onEdit, onDelete }) => {
+    const [sortBy, setSortBy] = useState<keyof IBank>('id');
+    const [sortOrder, setSortOrder] = useState<'asc' | 'desc'>('asc');
+
+    const handleSort = (column: keyof IBank) => {
+        if (sortBy === column) {
+            setSortOrder(sortOrder === 'asc' ? 'desc' : 'asc');
+        } else {
+            setSortBy(column);
+            setSortOrder('asc');
+        }
+    };
+
+    const sortedBanks = banks.sort((a: IBank, b: IBank) => {
+        const sortMultiplier = sortOrder === 'asc' ? 1 : -1;
+        if (a[sortBy]! < b[sortBy]!) return -1 * sortMultiplier;
+        if (a[sortBy]! > b[sortBy]!) return 1 * sortMultiplier;
+        return 0;
+    });
+
     return (
         <TableContainer component={Paper}>
             <Table>
                 <TableHead>
                     <TableRow>
-                        <TableCell>ID</TableCell>
-                        <TableCell>Bank Name</TableCell>
-                        <TableCell>Routing Number</TableCell>
-                        <TableCell>SWIFT/BIC</TableCell>
+                        <TableCell onClick={() => handleSort('id')}>ID</TableCell>
+                        <TableCell onClick={() => handleSort('bank_name')}>Bank Name</TableCell>
+                        <TableCell onClick={() => handleSort('routing_number')}>Routing Number</TableCell>
+                        <TableCell onClick={() => handleSort('swift_bic')}>SWIFT/BIC</TableCell>
                         <TableCell>Actions</TableCell>
                     </TableRow>
                 </TableHead>
                 <TableBody>
-                    {banks.map((bank: IBank) => (
+                    {sortedBanks.map((bank: IBank) => (
                         <TableRow key={bank.id}>
                             <TableCell>{bank.id}</TableCell>
                             <TableCell>{bank.bank_name}</TableCell>
--- frontend/src/components/UsersTable/index.tsx
+++ frontend/src/components/UsersTable/index.tsx
@@ -1,25 +1,44 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Table, TableBody, TableCell, TableContainer, TableHead, TableRow, IconButton, Paper } from '@mui/material';
 import EditIcon from '@mui/icons-material/Edit';
 import DeleteIcon from '@mui/icons-material/Delete';
 import { IUser, UsersTableProps } from '../../models/interfaces';
 
 const UsersTable: React.FC<UsersTableProps> = ({ users, onEdit, onDelete }) => {
+    const [sortBy, setSortBy] = useState<keyof IUser>('id');
+    const [sortOrder, setSortOrder] = useState<'asc' | 'desc'>('asc');
+
+    const handleSort = (column: keyof IUser) => {
+        if (sortBy === column) {
+            setSortOrder(sortOrder === 'asc' ? 'desc' : 'asc');
+        } else {
+            setSortBy(column);
+            setSortOrder('asc');
+        }
+    };
+
+    const sortedUsers = users.sort((a: IUser, b: IUser) => {
+        const sortMultiplier = sortOrder === 'asc' ? 1 : -1;
+        if (a[sortBy]! < b[sortBy]!) return -1 * sortMultiplier;
+        if (a[sortBy]! > b[sortBy]!) return 1 * sortMultiplier;
+        return 0;
+    });
+
     return (
         <TableContainer component={Paper}>
             <Table>
                 <TableHead>
                     <TableRow>
-                        <TableCell>ID</TableCell>
-                        <TableCell>Username</TableCell>
-                        <TableCell>First Name</TableCell>
-                        <TableCell>Last Name</TableCell>
-                        <TableCell>Email</TableCell>
+                        <TableCell onClick={() => handleSort('id')}>ID</TableCell>
+                        <TableCell onClick={() => handleSort('username')}>Username</TableCell>
+                        <TableCell onClick={() => handleSort('first_name')}>First Name</TableCell>
+                        <TableCell onClick={() => handleSort('last_name')}>Last Name</TableCell>
+                        <TableCell onClick={() => handleSort('email')}>Email</TableCell>
                         <TableCell>Actions</TableCell>
                     </TableRow>
                 </TableHead>
                 <TableBody>
-                    {users.map((user: IUser) => (
+                    {sortedUsers.map((user: IUser) => (
                         <TableRow key={user.id}>
                             <TableCell>{user.id}</TableCell>
                             <TableCell>{user.username}</TableCell>
